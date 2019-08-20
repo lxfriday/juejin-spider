d59051c44a6458ea44600a2cb044a808d8c580f1
lxfriday <liu3248184446@outlook.com>
2019-08-20 22:38:25 +0800

    chore: update 20190820

---
+++ src/calcUserDianzanRank.js
@@ -0,0 +1,53 @@
+/**
+ * 计算用户点赞量排名
+ * @author lxfriday
+ */
+const chalk = require('chalk')
+const { createHeap, findMaxPrev } = require('./utils/sortPrev')
+const travelArticleData = require('./utils/travelArticleData')
+const saveDataTofile = require('./utils/saveDataTofile')
+
+const idSet = new Set()
+const allArticleObj = {}
+
+function compareVal(a) {
+  return a.user.totalCollectionsCount || 0
+}
+
+const target = Array(1000).fill({ collectionCount: 0 })
+
+createHeap(target, compareVal)
+
+travelArticleData(articleInfo => {
+  const { objectId, user, collectionCount } = articleInfo
+  if (!idSet.has(objectId)) {
+    idSet.add(objectId)
+    allArticleObj[objectId] = objectId
+    console.log(
+      chalk.cyan(
+        `collectionCount => ${collectionCount}, user => ${user.username}, level => ${user.level}`
+      )
+    )
+
+    findMaxPrev(articleInfo, target, compareVal)
+  }
+})
+
+target.sort((a, b) => b.collectionCount - a.collectionCount)
+
+saveDataTofile('calcDianzanRank', `点赞rank.json`, target)
+
+// save as md
+function generateMd() {
+  const title = '# 点赞排行 \r\n\r\n'
+  let content = '👍 点赞数，📌 标签 \r\n'
+  target.forEach((v, i) => {
+    content += `- (${i + 1})[👍 ${v.collectionCount}][📌 ${v.tags[0].title}] [${
+      v.title
+    }](${v.originalUrl}) \r\n`
+  })
+
+  saveDataTofile('calcDianzanRank', `点赞rank.md`, title + content, false)
+}
+
+generateMd()
