a6ee104e2523e7cad917f9677e17d035f0950488
lxfriday <liu3248184446@outlook.com>
2019-08-26 00:40:15 +0800

    Updates

---
+++ 5.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[5],{RXBc:function(e,t,a){"use strict";var u=a("g09b");Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var d=u(a("q1tI")),n=u(a("lRau"));function o(){return d.default.createElement(n.default,{to:"/dashboard"})}var r=o;t.default=r}}]);
