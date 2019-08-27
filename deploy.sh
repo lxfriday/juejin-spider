957e7ab59ef49b88d7d0fde4757e08be3712adcc
lxfriday <liu3248184446@outlook.com>
2019-08-28 07:18:27 +0800

    build: add deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
 cd ./website/dist
+git init
+git add *
+git remote add origin git@gitee.com:lxfriday/juejin-spider.git
+git checkout -b ge-pages
+git commit -m "update: data"
+git push -u -f origin ge-pages
+
