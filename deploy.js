88e3050aeacc4c9c34cc2f19a79ab27c2010a5ef
lxfriday <liu3248184446@outlook.com>
2019-09-20 08:37:59 +0800

    fix: 定时执行

--- deploy.js
+++ deploy.js
@@ -10,21 +10,22 @@ const shell = require('shelljs')
 // 获取当前时间
 // http://quan.suning.com/getSysTime.do
 
-// eslint-disable-next-line
-;(async () => {
-  const { sysTime1 } = await request('http://quan.suning.com/getSysTime.do', {
-    json: true,
-  })
+setTimeout(() => {
+  // eslint-disable-next-line
+  ;(async () => {
+    const { sysTime1 } = await request('http://quan.suning.com/getSysTime.do', {
+      json: true,
+    })
+    const timeStr = sysTime1.substr(0, 8)
+    // git
+    shell.exec(`git add . && git commit -m "build: ${timeStr}"`)
+    shell.exec('git push')
+    shell.exec(`git tag ${timeStr}`)
+    shell.exec('git push --tags')
 
-  const timeStr = sysTime1.substr(0, 8)
-  // git
-  shell.exec(`git add . && git commit -m "build: ${timeStr}"`)
-  shell.exec('git push')
-  shell.exec(`git tag ${timeStr}`)
-  shell.exec('git push --tags')
-
-  shell.echo('-------')
-  shell.echo('start deploy')
-  shell.exec('sh ./deploy.sh')
-  shell.echo('pushed to gitee')
-})()
+    shell.echo('-------')
+    shell.echo('start deploy')
+    shell.exec('sh ./deploy.sh')
+    shell.echo('pushed to gitee')
+  })()
+}, 1800000)
